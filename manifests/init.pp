263ffd981ab174d4a8b220c1b881c75d76174416
Gareth Rushgrove <gareth@morethanseven.net>
2013-03-29 14:49:24 +0000

    add os checking as relies on deb file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,32 @@
 class scala {
-  include 'scala::package'
+  include wget
+
+  validate_re($::osfamily, '^Debian$', 'This module relies on a deb package and only works on Debian based distros')
+
+  wget::fetch { 'download-typesafe-repo':
+    source      => 'http://apt.typesafe.com/repo-deb-build-0002.deb',
+    destination => '/usr/local/src/repo-deb-build-0002.deb',
+    before      => Package['typesafe_repo'],
+  }
+
+  package { 'typesafe-repo':
+    ensure   => installed,
+    source   => '/usr/local/src/repo-deb-build-0002.deb',
+    provider => dpkg,
+    notify   => Exec['apt-update-typesafe'];
+  }
+
+  exec { 'apt-update-typesafe':
+    command     => '/usr/bin/apt-get update',
+    refreshonly => true;
+  }
+
+  package { [
+    'typesafe-stack',
+    'scala',
+  ]:
+    ensure  => installed,
+    require => Exec['apt-update-typesafe'],
+  }
+
 }
--- spec/classes/scala_spec.rb
+++ spec/classes/scala_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 describe 'scala', :type => :class do
-  it { should include_class('scala::package') }
+  let(:facts) { {:osfamily => 'Debian', :lsbdistcodename => 'precise'} } 
+
   it { should include_class('wget') }
   it { should contain_package('scala').with_ensure('installed') }
   it { should contain_package('typesafe-repo').with_ensure('installed') }
   it { should contain_package('typesafe-stack').with_ensure('installed') }
+
+  context 'with an invalid distro name' do
+    let(:facts) { {:osfamily => 'RedHat', :lsbdistcodename => 'centos'} }
+    it do
+      expect {
+        should contain_package('scala')
+      }.to raise_error(Puppet::Error, /This module relies on a deb package/)
+    end
+  end
+
 end
